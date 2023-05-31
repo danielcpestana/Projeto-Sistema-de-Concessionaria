2f86ca2e0c8181bb521452a8d311efebc3f61bb4
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2023-05-31 19:35:23 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -1,102 +1,81 @@
-from Projeto.lib.interface import *
+import re
+from lib.interface.interface import *
 
-def arquivoExisteCliente(nome):
-    try:
-        a = open(nome, 'rt')
-        a.close()
-    except FileNotFoundError:
+
+def formatar_cpf(cpf):
+    cpf = re.sub(r'\D', '', cpf)
+    # Formata o CPF com pontos e hífen
+    cpf_formatado = f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
+
+    return cpf_formatado
+
+
+def validar_cpf(cpf):
+    cpf = re.sub(r'\D', '', cpf)
+    if len(cpf) != 11:
         return False
-    else:
-        return True
-
-def criarArquivoCliente(nome):
-    try:
-        a = open(nome, 'wt+')
-        a.close()
-    except:
-        print('Houve um ERRO na criação do arquivo!')
-    else:
-        print(f'Arquivo {nome} criado com sucesso!')
-
-
-def lerArquivoCliente(nome):
-    try:
-        a = open(nome, 'rt')
-    except:
-        print('Erro ao ler arquivo!')
-    else:
-        cabeçalho('PESSOAS CADASTRADAS')
-        for linha in a:
-            dado = linha.split(';')
-            dado[1] = dado[1].replace('\n', '')
-            print(f'{dado[0]:<30}{dado[1]:>3} anos')
-    finally:
-        a.close()
-
-
-def cadastrarCliente(arq1, nome='desconhecido', cpf=0):
-    try:
-        a = open(arq1, 'at')
-    except:
-        print('Houve um ERRO na abertura do arquivo!')
-    else:
-        try:
-            a.write(f'{nome};{cpf}\n')
-        except:
-            print('Houve um ERRO na hora de escrever os dados!')
-        else:
-            print(f'Novo registro de {nome} adicionado.')
-            a.close()
-
-
-
-
-def arquivoExisteProduto(moto):
-    try:
-        a = open(moto, 'rt')
-        a.close()
-    except FileNotFoundError:
+    if cpf == cpf[0] * 11:
         return False
-    else:
-        return True
+    soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
+    resto = soma % 11
+    digito_verificador_1 = 0 if resto < 2 else 11 - resto
+    if int(cpf[9]) != digito_verificador_1:
+        return False
+    soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
+    resto = soma % 11
+    digito_verificador_2 = 0 if resto < 2 else 11 - resto
+    if int(cpf[10]) != digito_verificador_2:
+        return False
 
-def criarArquivoProduto(moto):
+    return True
+def cadastrar_cliente():
+    cabeçalho(" Cadastrar Cliente ")
     try:
-        a = open(moto, 'wt+')
-        a.close()
-    except:
-        print('Houve um ERRO na criação do arquivo!')
-    else:
-        print(f'Arquivo {moto} criado com sucesso!')
+        nome = input("Digite o nome do cliente: ").upper()
+        cpf = input("Digite o CPF do cliente: ")
+
+        # Valida o CPF
+        cpf_valido = validar_cpf(cpf)
+
+        if not cpf_valido:
+            print("CPF inválido. Por favor, digite novamente.")
+            return
+
+        # Formata CPF, telefone e endereço
+        cpf_formatado = formatar_cpf(cpf)
+
+        with open("clientes.txt", "a") as arquivo:
+            arquivo.write(f"{nome},{cpf_formatado}\n")
+
+        print("Cliente cadastrado com sucesso!")
+    except Exception as e:
+        print("Ocorreu um erro ao cadastrar o cliente:", str(e))
 
 
-def lerArquivoProduto(moto):
+
+def cadastrar_motocicleta():
+    cabeçalho(" Cadastrar Motocicleta ")
     try:
-        a = open(moto, 'rt')
-    except:
-        print('Erro ao ler arquivo!')
-    else:
-        cabeçalho('MOTOS CADASTRADAS')
-        for linha in a:
-            dado = linha.split(';')
-            dado[1] = dado[1].replace('\n', '')
-            print(f'{dado[0]:<30}{dado[1]:>3} anos')
-    finally:
-        a.close()
+        marca = input("Digite a marca da motocicleta: ").upper()
+        modelo = input("Digite o modelo da motocicleta: ").upper()
 
+        # Solicita o ano da motocicleta até que seja digitado um valor numérico
+        while True:
+            ano = input("Digite o ano da motocicleta: ")
+            if ano.isdigit():
+                break
+            print("Ano inválido. Digite apenas números.")
 
-def cadastrarProduto(arq2, marca_motocicleta='Desconhecido', modelo_motocicleta='Desconhecido', preco_motocicleta='Desconhecido'):
-    try:
-        a = open(arq2, 'at')
-    except:
-        print('Houve um ERRO na abertura do arquivo!')
-    else:
-        try:
-            a.write(f'{marca_motocicleta};{modelo_motocicleta},{preco_motocicleta}\n')
-        except:
-            print('Houve um ERRO na hora de escrever os dados!')
-        else:
-            print(f'Novo registro de {marca_motocicleta} adicionado.')
-            a.close()
+        # Solicita o preço da motocicleta até que seja digitado um valor numérico
+        while True:
+            preco = input("Digite o preço da motocicleta: ")
+            if preco.isdigit():
+                break
+            print("Preço inválido. Digite apenas números.")
 
+        with open("motocicletas.txt", "a") as arquivo:
+            arquivo.write(f"{marca},{modelo},{ano},{preco}\n")
 
+        print("Motocicleta cadastrada com sucesso!")
+    except Exception as e:
+        print("Ocorreu um erro ao cadastrar a motocicleta:", str(e))
