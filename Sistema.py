95dcfaa1f9ad4136fbef0a4d803fcd4a1f57b09a
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2023-06-29 20:29:39 -0300

    Update Sistema.py

--- Sistema.py
+++ Sistema.py
@@ -7,7 +7,17 @@ while True:
     if resp == 1:
         cadastrar_cliente()
     elif resp == 2:
+        # Opção de cadastrar uma motocicleta
         cadastrar_motocicleta()
+    elif resp == 3:
+        # Opção de realizar uma venda
+        realizar_venda()
+    elif resp == 4:
+        listar_clientes()
+    elif resp == 5:
+        listar_motocicletas_disponiveis()
+    elif resp == 6:
+        listar_vendas()
     elif resp == 7:
         break
     else:
